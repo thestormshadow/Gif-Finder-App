f744b6ae27eeb4d74aa68cc5ceaa6eb79b425136
Javier Vargas Ruiz <javiervargasruiz94@gmail.com>
2020-08-19 23:51:36 -0500

    add buscar docs build docs 2

---
+++ docs/precache-manifest.3c3cb106971a3a6af040a9be697f2f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89632b7393d37b00ce0525e45a5ea6d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a39662b9abefd293fd54",
+    "url": "/static/css/main.b6012c41.chunk.css"
+  },
+  {
+    "revision": "20587489744efe70ecbd",
+    "url": "/static/js/2.d3f4cc74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3f4cc74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39662b9abefd293fd54",
+    "url": "/static/js/main.b131d411.chunk.js"
+  },
+  {
+    "revision": "957204c3df52389fdfc9",
+    "url": "/static/js/runtime-main.f1787591.js"
+  }
+]);
