f60aae6fb67e226d2e272e78acebb55dde00a3c0
Javier Vargas Ruiz <javiervargasruiz94@gmail.com>
2020-08-19 22:58:56 -0500

    add docs

---
+++ docs/precache-manifest.860403c62863959a1df484610c4112a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1f21b962b27591a4296fa6db05d7b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e59f610445246ad8c3c",
+    "url": "/static/css/main.1b0b3bfb.chunk.css"
+  },
+  {
+    "revision": "20587489744efe70ecbd",
+    "url": "/static/js/2.d3f4cc74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3f4cc74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e59f610445246ad8c3c",
+    "url": "/static/js/main.b6d1b694.chunk.js"
+  },
+  {
+    "revision": "957204c3df52389fdfc9",
+    "url": "/static/js/runtime-main.f1787591.js"
+  }
+]);
